bbe05338bb561542370617312e760bb51267fd7c
srinivasGokarla <87422030+srinu217@users.noreply.github.com>
2022-04-14 11:35:45 +0530

    admin

---
+++ adminui/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import List from './Components/User';
+function App() {
+  return (
+    <div className="App">
+  <List />
+    </div>
+  );
+}
+
+export default App;
